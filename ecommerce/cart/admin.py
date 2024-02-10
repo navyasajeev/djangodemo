c213a576be03c59ff0cf57b656611d711e2be490
navyasajeev <navyasajeev2001@gmail.com>
2024-02-10 10:03:47 +0530

    added

---
+++ ecommerce/cart/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from cart.models import Cart,Order, Account
+
+
+
+# Register your models here.
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(Account)
